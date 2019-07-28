4000e94331d85d97fe0bf01fc1f803ee90d9acf8
Alexis David <alexis.david.mendez@gmail.com>
2019-07-28 17:25:27 -0400

    Add support for todos creation

--- src/modules/todos/components/TodoForm.tsx
+++ src/modules/todos/components/TodoForm.tsx
@@ -1,28 +1,53 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../redux/actions";
 
 let nextTodoId = 0;
 
 const TodoForm: React.FC = () => {
-  const ref = React.createRef<HTMLInputElement>();
+  const [fieldValue, setFieldValue] = useState("");
 
-  const handleClick = () => {};
+  const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    setFieldValue(evt.target.value);
+  };
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = useCallback(
+    (evt: React.FormEvent<HTMLFormElement>) => {
+      evt.preventDefault();
+      dispatch(
+        addTodo({
+          id: nextTodoId++,
+          text: fieldValue,
+          toggled: false,
+          remarked: false
+        })
+      );
+      setFieldValue("");
+    },
+    [fieldValue, dispatch]
+  );
 
   return (
-    <div className="field is-grouped">
-      <p className="control is-expanded">
-        <input
-          className="input"
-          type="text"
-          placeholder="Type a task"
-          ref={ref}
-        />
-      </p>
-      <p className="control">
-        <a className="button is-info" onClick={handleClick}>
-          Add
-        </a>
-      </p>
-    </div>
+    <form action="" onSubmit={handleSubmit}>
+      <div className="field is-grouped">
+        <p className="control is-expanded">
+          <input
+            className="input"
+            type="text"
+            placeholder="Type a task"
+            value={fieldValue}
+            onChange={handleChange}
+          />
+        </p>
+        <p className="control">
+          <button type="submit" className="button is-info">
+            Add
+          </button>
+        </p>
+      </div>
+    </form>
   );
 };
 

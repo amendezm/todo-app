0e81ccfb282966b9067bcdd5a519ed3261892c00
Alexis David <alexis.david.mendez@gmail.com>
2019-07-28 12:35:09 -0400

    Create todo actions

--- src/modules/todos/redux/actions.ts
+++ src/modules/todos/redux/actions.ts
@@ -1,1 +1,15 @@
-export {};
+import { createAction } from "./../../../lib/action-creators";
+
+const addTodo = createAction<{ id: number; text: string }>("ADD_TODO");
+
+const removeTodo = createAction<{ id: number }>("REMOVE_TODO");
+
+const toggleTodo = createAction<{ id: number }>("TOGGLE_TODO");
+
+const remarkTodo = createAction<{ id: number }>("REMARK_TODO");
+
+const setVisibilityFIlter = createAction<{ filter: string }>(
+  "SET_VISIBILITY_FILTER"
+);
+
+export { addTodo, removeTodo, toggleTodo, remarkTodo, setVisibilityFIlter };
